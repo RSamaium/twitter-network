dc915fd7e333724ed74cb61bf374abff99f12236
Samuel Ronce <sam@webcreative5.net>
2020-04-09 14:31:13 +0200

    edge fix

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express()
 function loadData() { 
     const file = fs.readFileSync(__dirname + '/data/fullEdges.json', 'utf-8')
     const json = JSON.parse(file)
-    json.map(f => g.addLink('' + f[0], '' + f[1]))
+    json.map(f => g.addLink('' + f[1], '' + f[0]))
 }
 
 console.log('Loading data...')
@@ -23,7 +23,9 @@ app.use(cors())
 
 app.post('/path', function (req, res) {
     const { start, end } = req.body
-    const pathFinder = ngraphPath.aStar(g)
+    const pathFinder = ngraphPath.aStar(g, {
+        oriented: true
+      })
     const foundPath = pathFinder.find(''+start, ''+end);
     res.json(foundPath.reverse().map(n => n.id))
 })
